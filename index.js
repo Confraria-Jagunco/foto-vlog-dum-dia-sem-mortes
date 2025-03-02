7a0e5d53b0ca2c6e29fd36e4b51139e54ea64754
Confraria Jagunço <jaguncocomunicationltda@gmail.com>
2025-03-02 11:55:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const body = document.getElementById("bodyHtml");
+const body = document.getElementById("body");
+
+const header = document.getElementById("header");
+const main = document.getElementById("main");
+const toConcert = document.getElementById("toConcert");
 
 const titleOne = document.getElementById("firstTitle");
 const titleTwo = document.getElementById("secondTitle");
 
 const letsGo = document.getElementById("letsGo");
+
+const cover = document.getElementById("cover");
+const introduction = document.getElementById("introduction");
 
 var index = 0;
 
@@ -19,7 +26,10 @@ function changeIndex () {
 };
 
 function goToPhotos () {
-  body.style.display = "none";
+  header.style.display = "none";
+  main.style.display = "none";
+  toConcert.style.display = "none";
+  cover.style.display = "flex";
 }
 
 setInterval(changeIndex, 5000);
