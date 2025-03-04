b531654effd66a56ce5657d68f9c3de3b96c23b8
Confraria Jagunço <jaguncocomunicationltda@gmail.com>
2025-03-04 12:39:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,29 +9,103 @@ const titleTwo = document.getElementById("secondTitle");
 
 const letsGo = document.getElementById("letsGo");
 
-const cover = document.getElementById("cover");
-const introduction = document.getElementById("introduction");
+
+
+const previousImage = document.getElementById("previousImage");
+const placeImages = document.getElementById("placeImages");
+const nextImage = document.getElementById("nextImage");
 
 var index = 0;
+var indexAlbum = 0;
 
-function changeIndex () {
-    titleOne.style.display = "none";
-    titleTwo.style.display = "flex";
-    index++;
-    if (index > 1) {
-      index = 0;
-      titleOne.style.display = "flex";
-      titleTwo.style.display = "none";
-    }
-};
+function changeIndex() {
+  titleOne.style.display = "none";
+  titleTwo.style.display = "flex";
+  index++;
+  if (index > 1) {
+    index = 0;
+    titleOne.style.display = "flex";
+    titleTwo.style.display = "none";
+  }
+}
 
-function goToPhotos () {
+function goToConcert() {
   header.style.display = "none";
   main.style.display = "none";
   toConcert.style.display = "none";
-  cover.style.display = "flex";
+  galery.style.display = "flex";
+}
+
+var album = [
+  "https://i.imgur.com/gLDfrLH.jpeg",
+  "https://i.imgur.com/8DHfItE.png",
+  "https://i.imgur.com/akSj7R5.jpeg",
+  "https://i.imgur.com/AGE1odA.png",
+  "https://i.imgur.com/Z6pNOQ6.png",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  ""
+];
+
+placeImages.src = album[indexAlbum];
+
+function showNextImage() {
+  indexAlbum++;
+  placeImages.src = album[indexAlbum];
+  if (indexAlbum > album.length - 1) {
+    indexAlbum = 0;
+    placeImages.src = album[indexAlbum];
+  }
+}
+
+function showPreviousImage() {
+  indexAlbum--;
+  placeImages.src = album[indexAlbum];
+  if (indexAlbum < 0) {
+    indexAlbum = album.length - 1;
+    placeImages.src = album[indexAlbum];
+  }
 }
 
 setInterval(changeIndex, 5000);
 
-letsGo.addEventListener("click", goToPhotos);
+letsGo.addEventListener("click", goToConcert);
+nextImage.addEventListener("click", showNextImage);
+previousImage.addEventListener("click", showPreviousImage);
